a6d2995980a95ae371bc38f49c3a11a1047dffe0
suryavamsi6 <d.suryavamsi@gmail.com>
2021-12-24 14:58:45 +0530

    Setup Todo Project with Form

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      //throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input ref={todoTextInputRef} type="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
